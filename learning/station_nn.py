8a31e1a27ffa92785512f5a4d06dc99b3829cb6a
Whitebells <sky.lasts.mirror@gmail.com>
2019-12-15 17:22:42 +0800

    proximity extrapolation

---
+++ learning/station_nn.py
@@ -0,0 +1,13 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+import tensorflow as tf
+import numpy as np
+import sklearn as sk
+
+
+
+
+
+
+model = keras.models.Sequential()
